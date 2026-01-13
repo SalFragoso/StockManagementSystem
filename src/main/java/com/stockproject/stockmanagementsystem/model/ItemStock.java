c4b27639cd8a38c52b1d6e3624fe6f6d669689f7
Salvador Mota Fragoso <salfragosm@gmail.com>
2026-01-12 20:38:45 -0500

    Add ItemStock entity Model

---
+++ src/main/java/com/stockproject/stockmanagementsystem/model/ItemStock.java
@@ -0,0 +1,61 @@
+package com.stockproject.stockmanagementsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ItemStock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    int quantity;
+    Double price;
+    String name;
+
+
+    //Empty constructor for JPA
+    public ItemStock(){}
+
+    public ItemStock(int quantity, Double price, String name){
+
+        this.quantity = quantity;
+        this.price = price;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/com/stockproject/stockmanagementsystem/StockManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockproject.stockmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
