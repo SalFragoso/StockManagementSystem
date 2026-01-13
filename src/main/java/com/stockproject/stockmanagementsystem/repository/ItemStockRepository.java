76e6ad7569fb21cdbac85898b0c508fc508b9738
Salvador Mota Fragoso <salfragosm@gmail.com>
2026-01-12 20:48:19 -0500

    Add ItemStockRepository

---
+++ src/main/java/com/stockproject/stockmanagementsystem/repository/ItemStockRepository.java
@@ -0,0 +1,9 @@
+package com.stockproject.stockmanagementsystem.repository;
+
+import com.stockproject.stockmanagementsystem.model.ItemStock;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemStockRepository extends CrudRepository<ItemStock, Long> {
+
+
+}
